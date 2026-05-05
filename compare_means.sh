154461f659b361478a432a47ccfee52f6409ba4e
Nathaniel Getachew <natugetachew@gmail.com>
2026-05-05 14:01:59 -0400

    gitignore refresh

--- compare_means.sh
+++
@@ -1,3 +0,0 @@
-sbatch --wrap="module load conda/2024.09 && conda activate /home/ngetach/.conda/envs/rocky9/2024.09/CS587 && python /home/ngetach/tml/compare_means.py" \
-    --account=debug --partition=scholar-debug --cpus-per-task=4 --nodes=1 --time=10:00 \
-    --output=slurm_logs/compare_means_%j.out
